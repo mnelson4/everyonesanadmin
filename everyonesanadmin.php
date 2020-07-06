2a38bae4f1ea87b92fcc72fd4c3ec9c212bb3970
Mike Nelson <michael@eventespresso.com>
2020-07-06 11:28:40 -0700

    fix fatal error when activating

--- everyonesanadmin.php
+++ everyonesanadmin.php
@@ -1,8 +1,8 @@
 <?php
 /**
  *
-Plugin Name: I am user 1
-Description:
+Plugin Name: Everyone's the Admin
+Description: Never log in again. Anyone visits the site is an administrator. Only use in isolated development.
 Version: 0.1
  */
 // add_filter(
@@ -12,14 +12,19 @@ Version: 0.1
 // 	},
 // 	100
 // );
+if( ! function_exists('wp_get_current_user')){
 
-function wp_get_current_user(){
-	wp_set_current_user( 1 );
-	global $current_user;
-	//var_dump($current_user);
-	return $current_user;
+	function wp_get_current_user(){
+		wp_set_current_user( 1 );
+		global $current_user;
+		//var_dump($current_user);
+		return $current_user;
+	}
 }
 
-function wp_validate_auth_cookie(){
-	return 1;
+
+if( ! function_exists('wp_validate_auth_cookie')){
+	function wp_validate_auth_cookie(){
+		return 1;
+	}
 }
